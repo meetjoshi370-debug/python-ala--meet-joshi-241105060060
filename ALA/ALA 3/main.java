28aa3e6e0b7e30faae1336c93c45c53afaa5edf7
meetjoshi370-debug <meetjoshi370@gmail.com>
2026-04-14 14:15:46 +0530

    Add files via upload

---
+++ ALA/ALA 3/main.java
@@ -0,0 +1,9 @@
+public class main { 
+    public static void main(String[] args) { 
+        String s1="apple", s2="apple"; 
+        if(s1.equals(s2)) 
+            System.out.println("Strings are equal"); 
+        else 
+            System.out.println("Strings are not equal"); 
+    } 
+} 
---
+++ ALA/ALA 3/string.py
@@ -0,0 +1,7 @@
+s1 = "apple"
+s2 = "apple"
+
+if s1 == s2:
+    print("Strings are equal")
+else:
+    print("Strings are not equal")
